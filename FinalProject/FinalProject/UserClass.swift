619071742d78fe57c1af66a7155f88929040e2fb
justinsmith1374 <JPSmith@student.fullsail.edu>
2019-04-04 12:32:52 -0400

    User class file created

---
+++ FinalProject/FinalProject/UserClass.swift
@@ -0,0 +1,15 @@
+//
+//  UserClas.swift
+//  FinalProject
+//
+//  Created by Justin Smith on 4/4/19.
+//  Copyright © 2019 FullSailUniversity. All rights reserved.
+//
+
+import Foundation
+
+class User
+{
+
+    
+}
