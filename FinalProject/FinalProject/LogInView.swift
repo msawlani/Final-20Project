58f5132b9dab8d738e2f47501f1494a814f70a49
Ktronn <viticopq12@gmail.com>
2019-04-11 13:50:54 -0400

    Revert "Revert "LogInView""
    This reverts commit ecf8ad213c590122e4c0451b466b714b1585a751.

---
+++ FinalProject/FinalProject/LogInView.swift
@@ -0,0 +1,20 @@
+//
+//  LogInView.swift
+//  FinalProject
+//
+//  Created by Victor  Perez on 4/11/19.
+//  Copyright © 2019 FullSailUniversity. All rights reserved.
+//
+
+import UIKit
+
+class LogInView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
