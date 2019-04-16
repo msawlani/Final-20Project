15c84a873633befe06a9678c8f2e433bea06e615
Ktronn <viticopq12@gmail.com>
2019-04-16 11:25:00 -0400

    Firebase Log in
    Login code was added for the firebase authentication pre built UI.

--- FinalProject/FinalProject/LogInView.swift
+++ FinalProject/FinalProject/LogInView.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseUI
 
 class LogInView: UIViewController {
     
@@ -14,18 +16,34 @@ class LogInView: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    @IBAction func OnClick(_ sender: UIButton) {
-        
-        
-        //Get the default UI object
-        
-        //Set ourselves as delegates
-        
-        //Get a reference to the auth UI view controller
-        
-        //Show it
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
+    @IBAction func loginTapped(_ sender: UIButton) {
+        //Get the default UI object
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        guard authUI != nil else{
+            //log error
+            return
+        }
+        //Set ourselves as delegates
+        authUI?.delegate = self
+        //Get a reference to the auth UI view controller
+        let authViewController = authUI!.authViewController()
+        //Show it
+        present(authViewController, animated: true, completion: nil)
+        }
 }
-
+extension UIViewController: FUIAuthDelegate{
+    private func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        //check if there was an error
+        if error != nil{
+            //log error
+            return
+        }
+        //to get user id : authDataResult?.user.uid
+        performSegue(withIdentifier: "GoToMain", sender: self)
+    }
+}
