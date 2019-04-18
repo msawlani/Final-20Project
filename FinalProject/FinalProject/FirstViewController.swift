9db1c2ec8c9985cfd0f541b052b6957fec2f5fe4
justinsmith1374 <JPSmith@student.fullsail.edu>
2019-04-18 12:15:58 -0400

    App reads from Firebase

--- FinalProject/FinalProject/FirstViewController.swift
+++ FinalProject/FinalProject/FirstViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import Firebase
 
 class FirstViewController: UIViewController {
-
+    
+    var ref:DatabaseReference?
+    var databaseHandle:DatabaseHandle?
+    
+    @IBOutlet weak var balanceText: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let ref = Database.database().reference()
+        
+        databaseHandle = ref.child("user1").child("account").observe(.childChanged, with: { (snapshot) in
+            
+            //Convert the info of the data into a string variable
+            //let getData = snapshot.value as? String
+            //print(getData)
+            
+            let balance = snapshot.value as? String
+            var balanceString = "Balance: "
+            balanceString.append(balance!)
+            self.balanceText.text = balanceString
+        })
+        
+        ref.child("user1").child("username").setValue("user1")
+        ref.child("user1").child("account").child("accountName").setValue("account1")
+        ref.child("user1").child("account").child("balance").setValue("999")
+
+
     }
-
-
 }
 
--- FinalProject/FinalProject/SecondViewController.swift
+++ FinalProject/FinalProject/SecondViewController.swift
@@ -7,19 +7,12 @@
 //
 
 import UIKit
-import Firebase
 
 class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let ref = Database.database().reference()
-        
-        ref.child("user1").child("username").setValue("user1")
-        ref.child("user1").child("account").child("accountName").setValue("account1")
-        ref.child("user1").child("account").child("balance").setValue(999)
-        
         
     }
 
