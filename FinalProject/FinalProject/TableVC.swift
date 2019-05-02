879ec4dc17545b4c44d082e30db5e98ef4df61af
Ktronn <viticopq12@gmail.com>
2019-05-02 10:56:49 -0400

    Cards
    Base set up for default table view has been started. Two of the cells have been created but not finished.

---
+++ FinalProject/FinalProject/TableVC.swift
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.swift
+//  FinalProject
+//
+//  Created by Victor  Perez on 4/4/19.
+//  Copyright © 2019 FullSailUniversity. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+   
+
+}
+
---
+++ FinalProject/TableViewCell1.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell1.swift
+//  
+//
+//  Created by Victor  Perez on 5/2/19.
+//
+
+import UIKit
+
+class TableViewCell1: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
