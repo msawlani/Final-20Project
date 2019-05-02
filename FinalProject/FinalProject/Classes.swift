819940a19802f5a5bf4149a936cebc9c6b47e375
justinsmith1374 <JPSmith@student.fullsail.edu>
2019-05-02 11:35:18 -0400

    Revert "Storin g to firebase works, working on retrieving"
    This reverts commit fc4165f4611275736f86b8645a947cab710171fb.

--- FinalProject/FinalProject/Classes.swift
+++ FinalProject/FinalProject/Classes.swift
@@ -9,12 +9,9 @@
 import Foundation
 import Firebase
 
-var mRef:DatabaseReference?
-
-class User: NSObject
+class User
 {
     var username, password: String
-    var numAccounts: Int
     var accounts: [Account] = []
     var ref:DatabaseReference?
     
@@ -22,7 +19,6 @@ class User: NSObject
     {
         self.username = username
         self.password = password
-        self.numAccounts = 0
     }
     
     func AddAccount(account:Account) -> Bool
@@ -36,7 +32,6 @@ class User: NSObject
         }
         
         self.accounts.append(account)
-        self.numAccounts+=1
         return true
     }
     
@@ -49,10 +44,9 @@ class User: NSObject
             if self.accounts[i].name == accountName
             {
                 self.accounts.remove(at: i)
-                self.numAccounts-=1
                 return true
             }
-            i+=1
+            i = i + 1
         }
         
         return false
@@ -63,117 +57,43 @@ class User: NSObject
         let ref = Database.database().reference()
         ref.child("users").child(self.username).child("username").setValue(self.username)
         ref.child("users").child(self.username).child("password").setValue(self.password)
-        ref.child("users").child(self.username).child("numAccounts").setValue(self.accounts.count)
         
         var i = 0
         
         while i < self.accounts.count
         {
-                ref.child("users").child(self.username).child("accounts").child("accountNum" + "\(i)").child("accountNum").setValue("account" + "\(i)")
-                ref.child("users").child(self.username).child("accounts").child("accountNum" + "\(i)").child("accountName").setValue(self.accounts[i].name)
-                ref.child("users").child(self.username).child("accounts").child("accountNum" + "\(i)").child("balance").setValue(self.accounts[i].balance)
-                ref.child("users").child(self.username).child("accounts").child("accountNum" + "\(i)").child("bankName").setValue(self.accounts[i].bankName)
-                ref.child("users").child(self.username).child("accounts").child("accountNum" + "\(i)").child("numTransactions").setValue(self.accounts[i].transactions.count)
-            
-            var j = 0
-            
-            while j < self.accounts[i].transactions.count
-            {
-                ref.child("users").child(self.username).child("accounts").child(self.accounts[i].name).child("transactions").child("transaction" + "\(j)").child("name").setValue(self.accounts[i].transactions[j].name)
-                
-                ref.child("users").child(self.username).child("accounts").child(self.accounts[i].name).child("transactions").child("transaction" + "\(j)").child("category").setValue(self.accounts[i].transactions[j].category)
-                
-                ref.child("users").child(self.username).child("accounts").child(self.accounts[i].name).child("transactions").child("transaction" + "\(j)").child("description").setValue(self.accounts[i].transactions[j].description)
-                
-                ref.child("users").child(self.username).child("accounts").child(self.accounts[i].name).child("transactions").child("transaction" + "\(j)").child("amount").setValue(self.accounts[i].transactions[j].amount)
-                
-                j+=1
-            }
-            i+=1
+            ref.child("users").child(self.username).child("accounts").child(self.accounts[i].name).child("accountName").setValue(self.accounts[i].name)
+            ref.child("users").child(self.username).child("accounts").child(self.accounts[i].name).child("balance").setValue(self.accounts[i].balance)
+            i = i + 1
         }
-    }
-    
-    func ReadFromFirebase()
-    {
-        self.ref = Database.database().reference()
-        
-        ref?.child("users").child("testUser2").observe(.value, with: { (snapshot) in
-
-            do {
-                self.numAccounts = snapshot.childSnapshot(forPath: "numAccounts").value! as! Int
-            }
-            catch {
-                
-            }
-            
-        })
-//        var dataRead = false
-//
-//        ref?.child("users").child("testUser2")
-//            .observeSingleEvent(of: .value, with: { (snapshot) in
-//
-//                let userDict = snapshot.value as! [String: Any]
-//
-//                self.numAccounts = userDict["numAccounts"] as! Int
-//                self.password = userDict["password"] as! String
-//                dataRead = true
-//            })
-//
-//        if dataRead
-//        {
-//            var i = 0
-//
-//            while i < self.numAccounts
-//            {
-//                ref?.child("users").child("testUser2").child("accountNum" + "\(i)")
-//                    .observeSingleEvent(of: .value, with: { (snapshot) in
-//
-//                        let userDict = snapshot.value as! [String: Any]
-//
-//                        let tempName = userDict["accountName"] as! String
-//                        var tempAccount = Account(name: tempName)
-//                        tempAccount.balance = userDict["balance"] as! Float
-//                        tempAccount.bankName = userDict["bankName"] as! String
-//                        tempAccount.numTransactions = userDict["numTransactions"] as! Int
-//
-//                        self.AddAccount(account: tempAccount)
-//                    })
-//
-//                i+=1
-//            }
-//        }
     }
 }
 
 class Account
 {
-    var name, bankName, accountNum:String
-    var numTransactions: Int
+    var name, bankName: String
     var balance: Float
     var transactions: [Transaction] = []
     
-    init(name:String = "", accountNumber: Int = 0, bankName:String = "", balance:Float = 0.0)
+    init(name:String, bankName:String = "", balance:Float = 0.0)
     {
         self.name = name
-        self.accountNum = "account" + "\(accountNumber)"
         self.bankName = bankName
         self.balance = balance
-        self.numTransactions = 0
     }
     
     func AddTransaction(transaction: Transaction)
     {
         self.transactions.append(transaction)
         self.balance = self.balance + transaction.amount
-        self.numTransactions+=1
     }
     
     func RemoveTransaction(index: Int)
     {
         self.balance = self.balance - self.transactions[index].amount
         self.transactions.remove(at: index)
-        self.numTransactions-=1
     }
+    
 }
 
 class Transaction
@@ -219,51 +139,5 @@ struct Date
         self.day = day
         self.year = year
     }
-}
-
-func GetUser(username: String) -> User {
-    var user = User(username: username)
-    //mRef = Database.database().reference()
-    
-//    Database.database().reference().child("users").child(username).observe(.childAdded, with: {(snapshot) in
-//
-//        print(snapshot)
-//
-//    }, withCancel: nil)
-    
-    Database.database().reference().child("users").child(username).observeSingleEvent(of: .value, with: { (snapshot) in
-        //print(snapshot.value)
-        if let dictionary = snapshot.value as? [String: AnyObject] {
-            let user = User(username: username)
-            //user.setValuesForKeys(dictionary)
-            //print(snapshot)
-            //print(dictionary)
-            //print(dictionary["accountNum0"]?["balance"])
-            if let temp = dictionary["numAccounts"] as? Int{
-                //print(temp)
-                user.numAccounts = temp
-            }
-            if let temp = dictionary["password"] as? String{
-                user.password = temp
-            }
-            //print(user)
-//            user.numAccounts = snapshot.childSnapshot(forPath: "numAccounts").value! as! Int
-//            user.password = snapshot.childSnapshot(forPath: "password").value! as! String
-//
-//            var i = 0
-//            while i < user.numAccounts
-//            {
-//                var accountSnapshot = snapshot.childSnapshot(forPath:"accounts").childSnapshot(forPath: "accountNum" + "\(i)")
-//                var account = Account()
-//                account.name = accountSnapshot.childSnapshot(forPath: "accountName").value! as! String
-//                account.balance = accountSnapshot.childSnapshot(forPath: "balance").value! as! Float
-//                account.bankName = accountSnapshot.childSnapshot(forPath: "bankName").value! as! String
-//                account.numTransactions = accountSnapshot.childSnapshot(forPath: "numTransactions").value! as! Int
-//                user.AddAccount(account: account)
-//            }
-        }
-    })
-    //print(user.numAccounts)
-    return user
 }
 
--- FinalProject/FinalProject/FirstViewController.swift
+++ FinalProject/FinalProject/FirstViewController.swift
@@ -14,7 +14,6 @@ class FirstViewController: UIViewController {
     var ref:DatabaseReference?
     var databaseHandle:DatabaseHandle?
     var balance = 0
-    var user3 = User(username: "testUser2")
     
     @IBOutlet weak var balanceText: UILabel!
     
@@ -23,47 +22,36 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-//        let ref = Database.database().reference()
-//
-//        databaseHandle = ref.child("user1").child("account").observe(.childChanged, with: { (snapshot) in
-//
-//            //Convert the info of the data into a string variable
-//            //let getData = snapshot.value as? String
-//            //print(getData)
-//
-//            let amount = snapshot.value as? String
-//            var balanceString = "Balance: "
-//            balanceString.append(amount!)
-//            self.balanceText.text = balanceString
-//        })
+        let ref = Database.database().reference()
         
-        //ref.child("user1").child("username").setValue("user1")
-        //ref.child("user1").child("account").child("accountName").setValue("account1")
-        //ref.child("user1").child("account").child("balance").setValue(String(balance))
+        databaseHandle = ref.child("user1").child("account").observe(.childChanged, with: { (snapshot) in
+            
+            //Convert the info of the data into a string variable
+            //let getData = snapshot.value as? String
+            //print(getData)
+            
+            let amount = snapshot.value as? String
+            var balanceString = "Balance: "
+            balanceString.append(amount!)
+            self.balanceText.text = balanceString
+        })
+        
+        ref.child("user1").child("username").setValue("user1")
+        ref.child("user1").child("account").child("accountName").setValue("account1")
+        ref.child("user1").child("account").child("balance").setValue(String(balance))
         
         var user2 = User(username: "testUser2")
         var account1 = Account(name: "testAccount1", balance: 589.0)
-        account1.bankName = "Fake Bank"
         user2.AddAccount(account: account1)
         
-        var account2 = Account(name: "testAccount2", balance: 666.0)
-        account2.bankName = "Faker Bank"
-        user2.AddAccount(account: account2)
-        
         user2.StoreInFirebase()
-        
-        user3.ReadFromFirebase()
-        print(user3.numAccounts)
 
     }
     
     @IBAction func addToBalance(_ sender: Any) {
         let ref = Database.database().reference()
-        user3.username = "user3"
-        user3.password = "password"
-        //print(user3)
-        user3.StoreInFirebase()
-        
+        self.balance = balance + 100
+        ref.child("user1").child("account").child("balance").setValue(String(balance))
     }
 }
 
