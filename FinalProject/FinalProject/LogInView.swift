aef5741d6a51a89b92d5c8becdf168626d1b51e4
Michael <sawlani.michael@yahoo.com>
2019-05-06 13:02:34 -0400

    Login button
    Added login button to loginview and allowed to have action

--- FinalProject/FinalProject/LogInView.swift
+++ FinalProject/FinalProject/LogInView.swift
@@ -21,11 +21,18 @@ class LogInView: UIViewController,GIDSignInUIDelegate {
         googleButton.frame =  CGRect(x:16,y:650,width: view.frame.width - 32,height:50)
         view.addSubview(googleButton)
         GIDSignIn.sharedInstance()?.uiDelegate = self
-       
+        
+        let loginButton = UIButton.init()
+        loginButton.frame = CGRect(x: 16, y: 590, width: view.frame.width - 32, height: 50)
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.backgroundColor = UIColor.black
+        loginButton.tintColor = UIColor.white
+        loginButton.addTarget(self, action: #selector(toMain(_:)), for: .touchUpInside)
+        view.addSubview(loginButton)
     //googleButton.addTarget(self, action: #selector(toMain), for: .touchUpInside)
         
     }
-    @objc public func toMain(){
+    @objc public func toMain(_ :UIButton){
         performSegue(withIdentifier: "toMain", sender: self)
     }
     
