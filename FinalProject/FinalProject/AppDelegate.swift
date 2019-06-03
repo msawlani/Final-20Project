49ab1cbd9e79ac0a59488dd19dfc49b2e82b687b
Michael <sawlani.michael@yahoo.com>
2019-06-03 14:18:53 -0400

    Fixed Crash
    Fixed the core data crash

--- FinalProject/FinalProject/AppDelegate.swift
+++ FinalProject/FinalProject/AppDelegate.swift
@@ -86,7 +86,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
     }
 
     lazy var persistentContainer: NSPersistentContainer =  {
-        let container = NSPersistentContainer(name: "Core_Data")
+        let container = NSPersistentContainer(name: "CoreData")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError?{
                 fatalError("Unresolved \(error), \(error.userInfo)")
