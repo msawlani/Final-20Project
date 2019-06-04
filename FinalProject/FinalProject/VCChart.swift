7eaebb7e6e4e7ff19fe474b0f30a8d1f970ea538
Ktronn <viticopq12@gmail.com>
2019-06-04 13:12:54 -0400

    chart set up
    A new view was created to contain a pichart. Also a new filewas added called VCChart to manage the view.

---
+++ FinalProject/FinalProject/VCChart.swift
@@ -0,0 +1,38 @@
+//
+//  VCChart.swift
+//  FinalProject
+//
+//  Created by Victor  Perez on 6/4/19.
+//  Copyright © 2019 FullSailUniversity. All rights reserved.
+//
+
+import UIKit
+import Charts
+class VCChart: UIViewController {
+
+    @IBOutlet weak var pieChart: PieChartView!
+    
+    @IBOutlet weak var stepperOne: UIStepper!
+    
+    @IBOutlet weak var steppertTwo: UIStepper!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    @IBAction func changeStepperOne(_ sender: UIStepper){
+        
+    }
+    
+    @IBAction func changeStepperTwo(_ sender: UIStepper){
+        
+    }
+    
+    func updateChartData(){
+        
+    }
+
+}
