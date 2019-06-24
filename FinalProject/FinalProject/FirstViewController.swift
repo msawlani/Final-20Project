e7a52dddf00dd9694f1259659c184ee646811584
justinsmith1374 <JPSmith@student.fullsail.edu>
2019-06-24 16:42:39 -0400

    Yet again think I fixed deleting transactions

--- FinalProject/FinalProject/FirstViewController.swift
+++ FinalProject/FinalProject/FirstViewController.swift
@@ -189,8 +189,8 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
                 self.transactionArray[indexPath.section].TransactionList.remove(at: indexPath.row)
                 self.balanceText.text = String(format: "$%.02f", mainUser.accounts[0].balance)
                 self.Table.reloadData()
-                self.viewDidAppear(true)
-                
+                self.viewDidLoad()
+                self.viewWillAppear(true)
             }))
 
             alert.addAction(UIAlertAction(title: "No", style: .default, handler: nil))
